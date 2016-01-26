9c727770daf7bcfd2879feb0ec38075a72890688
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-01-25 23:33:00 -0800

    added basic test report

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# circuit
+[![Build Status](https://travis-ci.org/drom/logidrom.svg?branch=master)](https://travis-ci.org/drom/logidrom)
+[![Coverage Status](https://coveralls.io/repos/drom/logidrom/badge.svg?branch=master&service=github)](https://coveralls.io/github/drom/logidrom?branch=master)
+[![devDependency Status](https://david-dm.org/drom/logidrom/dev-status.svg)](https://david-dm.org/drom/logidrom#info=devDependencies)
 
-[![Build Status](https://travis-ci.org/drom/logidrom.svg?branch=master)](https://travis-ci.org/drom/circuit)
-[![Coverage Status](https://coveralls.io/repos/drom/logidrom/badge.svg?branch=master&service=github)](https://coveralls.io/github/drom/circuit?branch=master)
-[![devDependency Status](https://david-dm.org/drom/logidrom/dev-status.svg)](https://david-dm.org/drom/circuit#info=devDependencies)
+# LogiDrom
 
-Digital Circuit rendering engine
+Digital logic circuit rendering engine
 
 [styles](ref/styles.md)
--- test/basic.js
+++ test/basic.js
@@ -1,14 +1,47 @@
 'use strict';
 
-var lib = require('../lib'),
-    and_gate = require('./and_gate.json'),
+var lib = require('../lib/render-assign'),
+    fs = require('fs'),
+    onml = require('onml'),
+    // jsof = require('jsof'),
+    esprima = require('esprima'),
     expect = require('chai').expect;
 
+var w3 = {
+    svg: 'http://www.w3.org/2000/svg',
+    xlink: 'http://www.w3.org/1999/xlink',
+    xmlns: 'http://www.w3.org/XML/1998/namespace'
+};
+
+var dat = {
+    'and' : [['x', ['&', 'a', 'b']]],
+    'and5': [['x', ['&', 'a', 'b', 'c', 'd', 'e']]],
+    'or'  : [['x', ['|', 'a', 'b']]],
+    'xor'  : [['x', ['^', 'a', 'b']]],
+    'nand'  : [['x', ['~&', 'a', 'b']]],
+    'nor'  : [['x', ['~|', 'a', 'b']]],
+    // 'and' : 'x = a & b',
+    // 'and5': 'x = a & b & c & d & e',
+    // 'or'  : 'x = a | b',
+    // 'xor' : 'x = a ^ b',
+    // 'nand': 'x = ~(a & b)',
+    // 'nor' : 'x = ~(a | b)'
+};
+
 describe('basic', function () {
-    it('and_gate', function (done) {
-        var res;
-        res = lib(0, { assign: [['out', ['&', 'a', 'b']]]});
-        expect(res).to.be.deep.equal(and_gate);
-        done();
+    it('basic', function (done) {
+        var res = '<html><body>';
+        Object.keys(dat).forEach(function (t, index) {
+            var src = dat[t]
+            res += '<pre>' + JSON.stringify(src) + '</pre>\n';
+            // res += '<pre>' + src + '</pre>\n';
+            // var ast = esprima.parse(src);
+            // var dst = lib(ast, {index: index});
+            var dst = lib(index, {assign: src });
+            var svg = ['svg', {id: 'svgcontent_' + index, xmlns: w3.svg, 'xmlns:xlink': w3.xlink, overflow:'hidden'}, ['style', '.pinname {font-size:12px; font-style:normal; font-variant:normal; font-weight:500; font-stretch:normal; text-align:center; text-anchor:end; font-family:Helvetica} .wirename {font-size:12px; font-style:normal; font-variant:normal; font-weight:500; font-stretch:normal; text-align:center; text-anchor:start; font-family:Helvetica} .wirename:hover {fill:blue} .gate {color:#000; fill:#ffc; fill-opacity: 1;stroke:#000; stroke-width:1; stroke-opacity:1} .gate:hover {fill:red !important; } .wire {fill:none; stroke:#000; stroke-width:1; stroke-opacity:1} .grid {fill:#fff; fill-opacity:1; stroke:none}'], dst];
+            res += onml.stringify(svg) + '\n';
+        });
+        res += '</body></html>';
+        fs.writeFile('report.html', res, {encoding: 'utf8'}, done);
     });
 });
