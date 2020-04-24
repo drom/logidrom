4d614384c48feb0317a7a5a73314d3da2c2e4a6a
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2020-04-23 21:40:13 -0700

    backport form wavedrom

--- test/basic.js
+++ test/basic.js
@@ -1,17 +1,10 @@
 'use strict';
 
-var lib = require('../lib/render-assign');
+var lib = require('../lib/');
 var fs = require('fs');
 var onml = require('onml');
-// var jsof = require('jsof');
-// var esprima = require('esprima');
-// var expect = require('chai').expect;
-
-var w3 = {
-    svg: 'http://www.w3.org/2000/svg',
-    xlink: 'http://www.w3.org/1999/xlink',
-    xmlns: 'http://www.w3.org/XML/1998/namespace'
-};
+var chai = require('chai');
+var expect = chai.expect;
 
 var dat = {
     'and'  : [['x', ['&', 'a', 'b']]],
@@ -24,20 +17,23 @@ var dat = {
 };
 
 describe('basic', function () {
-    it('basic', function (done) {
-        var res = '<html><body>';
-        Object.keys(dat).forEach(function (t, index) {
-            var src = dat[t];
+    it('renderAssign is function', function (done) {
+        expect(lib.renderAssign).to.be.a('function');
+        done();
+    });
+    var res = '<html><body>';
+    Object.keys(dat).map(function (key, index) {
+        it(key, function (done) {
+            var src = dat[key];
             res += '<pre>' + JSON.stringify(src) + '</pre>\n';
-            // res += '<pre>' + src + '</pre>\n';
-            // var ast = esprima.parse(src);
-            // var dst = lib(ast, {index: index});
-            var dst = lib(index, {assign: src });
-            var svg = ['svg', {id: 'svgcontent_' + index, xmlns: w3.svg, 'xmlns:xlink': w3.xlink, overflow:'hidden'}, ['style', '.pinname {font-size:12px; font-style:normal; font-variant:normal; font-weight:500; font-stretch:normal; text-align:center; text-anchor:end; font-family:Helvetica} .wirename {font-size:12px; font-style:normal; font-variant:normal; font-weight:500; font-stretch:normal; text-align:center; text-anchor:start; font-family:Helvetica} .wirename:hover {fill:blue} .gate {color:#000; fill:#ffc; fill-opacity: 1;stroke:#000; stroke-width:1; stroke-opacity:1} .gate:hover {fill:red !important; } .wire {fill:none; stroke:#000; stroke-width:1; stroke-opacity:1} .grid {fill:#fff; fill-opacity:1; stroke:none}'], dst];
+            var svg = lib.renderAssign(index, {assign: src });
             res += onml.stringify(svg) + '\n';
+            done();
         });
+    });
+    after(function () {
         res += '</body></html>';
-        fs.writeFile('report.html', res, {encoding: 'utf8'}, done);
+        fs.writeFileSync('report.html', res, {encoding: 'utf8'});
     });
 });
 
