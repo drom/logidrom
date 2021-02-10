71ac12c38519d5d2f68d6345f4c658a78f046f4d
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2021-02-09 16:41:08 -0800

    var->const,let cleanup

--- test/basic.js
+++ test/basic.js
@@ -1,40 +1,50 @@
 'use strict';
 
-var lib = require('../lib/');
-var fs = require('fs');
-var onml = require('onml');
-var chai = require('chai');
-var expect = chai.expect;
+const fs = require('fs');
+const onml = require('onml');
+const chai = require('chai');
+
+const lib = require('../lib/');
+
+const expect = chai.expect;
 
 var dat = {
-    'and'  : [['x', ['&', 'a', 'b']]],
-    'and5' : [['x', ['&', 'a', 'b', 'c', 'd', 'e']]],
-    'or'   : [['x', ['|', 'a', 'b']]],
-    'xor'  : [['x', ['^', 'a', 'b']]],
-    'nand' : [['x', ['~&', 'a', 'b']]],
-    'nor'  : [['x', ['~|', 'a', 'b']]],
-    'oa1'  : [['x', ['~|', 'a0', 'a1', 'a2', ['~&', 'a3', 'a4', 'a5']]]]
+  'and'  : [['x', ['&', 'a', 'b']]],
+  'and5' : [['x', ['&', 'a', 'b', 'c', 'd', 'e']]],
+  'or'   : [['x', ['|', 'a', 'b']]],
+  'xor'  : [['x', ['^', 'a', 'b']]],
+  'nand' : [['x', ['~&', 'a', 'b']]],
+  'nor'  : [['x', ['~|', 'a', 'b']]],
+  'oa1'  : [['x', ['~|', 'a0', 'a1', 'a2', ['~&', 'a3', 'a4', 'a5']]]],
+  'const': [['foo', [42, 5]]]
 };
 
 describe('basic', function () {
-    it('renderAssign is function', function (done) {
-        expect(lib.renderAssign).to.be.a('function');
-        done();
+  it('renderAssign is function', function (done) {
+    expect(lib.renderAssign).to.be.a('function');
+    done();
+  });
+
+  it('fir is function', function (done) {
+    expect(lib.fir).to.be.a('function');
+    done();
+  });
+
+  let res = '<html><head><style>body {background-color: #eee; }</style></head><body>';
+  Object.keys(dat).map(function (key, index) {
+    it(key, function (done) {
+      const src = dat[key];
+      res += '<pre>' + JSON.stringify(src, null, 4) + '</pre>\n';
+      var svg = lib.renderAssign(index, {assign: src });
+      res += onml.stringify(svg, 2) + '\n';
+      res += '<hr>';
+      done();
     });
-    var res = '<html><body>';
-    Object.keys(dat).map(function (key, index) {
-        it(key, function (done) {
-            var src = dat[key];
-            res += '<pre>' + JSON.stringify(src) + '</pre>\n';
-            var svg = lib.renderAssign(index, {assign: src });
-            res += onml.stringify(svg) + '\n';
-            done();
-        });
-    });
-    after(function () {
-        res += '</body></html>';
-        fs.writeFileSync('report.html', res, {encoding: 'utf8'});
-    });
+  });
+  after(function () {
+    res += '</body></html>';
+    fs.writeFileSync('report.html', res, {encoding: 'utf8'});
+  });
 });
 
 /* eslint-env mocha */
